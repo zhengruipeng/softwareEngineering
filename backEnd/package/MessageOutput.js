145b967d48e009bb2ee82fd80ea2af048c4d244d
miki <37892435+zhengruipeng@users.noreply.github.com>
2023-06-28 14:55:28 +0800

    Delete backEnd directory

--- backEnd/package/MessageOutput.js
+++
@@ -1,9 +0,0 @@
-class MessageOutput{
-    static output(msg,limit = msg.length/*int*/){
-        if(msg.length>limit)msg = msg.substring(0,limit);
-        console.log(msg);
-    };
-}
-
-module.exports = MessageOutput
-// export {MessageOutput}
